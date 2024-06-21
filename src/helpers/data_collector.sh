fd78c30a89710ec78ca6485eed9df945d8e42051
Dmitry <kor.dima97@mail.ru>
2024-06-21 14:22:18 +0500

    Запись ошибок

--- src/helpers/data_collector.sh
+++ src/helpers/data_collector.sh
@@ -1,7 +1,7 @@
 # Получение дампа БД
 function db_collect() {
   mkdir -p temporary_files/DB
-  docker exec "$CONTAINER_NAME" mysqldump -u "$DB_USERNAME" --password="$DB_PASSWORD" "$DB_DATABASE" > temporary_files/DB/dump.sql 2>&1
+  docker exec "$CONTAINER_NAME" mysqldump -u "$DB_USERNAME" --password="$DB_PASSWORD" "$DB_DATABASE" > temporary_files/DB/dump.sql 2>>logs/errors.log
 }
 
 # Получение файлов
